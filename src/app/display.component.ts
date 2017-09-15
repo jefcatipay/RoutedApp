7589b505c8ed1add3c96d64dcf5ffd03893208e8
japps1c22 <japps1c22@cicct.local>
2017-09-15 09:21:59 +0800

    Initial

---
+++ src/app/display.component.ts
@@ -0,0 +1,18 @@
+OnInit {
+
+    studNo: string;
+    studFname: string;
+    studLname: string;
+    studProg: string;
+    studYr: string;
+}
+
+constructor(private common: ){ }
+
+ngOnInit() {
+    this.studNo =
+    this.studFname =
+    this.studLname =
+    this.studProg = 
+    this.studYr
+}
